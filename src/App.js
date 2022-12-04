ce21f93bea8f6d0e665bf5df3296b41adbe50de8
John-Kimani <kimwaweru100@gmail.com>
2022-12-04 11:42:02 +0300

    App: Create not found page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 
-import { BrowserRouter, Routes, Route, Redirect } from "react-router-dom"
+import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Cart from './components/cart/Cart';
 import Home from './components/home/Home';
 import NavbarComponent from './components/navbar/Navbar';
+import PageNotFound404 from './components/errors/pagenotfound/404_page_not_found';
 
 
 function App() {
@@ -16,7 +17,8 @@ function App() {
         <NavbarComponent />
         <Routes>
           <Route path='/cart' element={<Cart />} />
-          <Route path='/' element={<Home />} />
+          <Route exact path='/' element={<Home />} />
+          <Route path='*' element={<PageNotFound404 />}/>
         </Routes>
       </BrowserRouter>
     </div>
---
+++ src/components/errors/pagenotfound/404_page_not_found.jsx
@@ -0,0 +1,36 @@
+import "./404_page_not_found.css";
+import { Link } from "react-router-dom";
+import { Button } from "react-bootstrap";
+
+const PageNotFound404 = () => {
+  return (
+    <div className="container container-fluid">
+      <div className="row mt-5">
+        <div className="col-md-6 col-lg-6">
+          <aside>
+            <img
+              src="https://s3-us-west-2.amazonaws.com/s.cdpn.io/4424790/Mirror.png"
+              alt="404"
+            />
+          </aside>
+        </div>
+
+        <div className="col-md-6 col-lg-6">
+          <main className="pt-4">
+            <h1>Sorry!</h1>
+            <p>
+              Either you aren't cool enough to visit this page or it doesn't
+              exist <em>. . . like your social life.</em>
+            </p>
+            <Link to="/">
+              <Button className="back_to_home">Back to Home Page</Button>
+            </Link>
+          </main>
+        </div>
+      </div>
+      <div className="content"></div>
+    </div>
+  );
+};
+
+export default PageNotFound404;
